b7d6df261326c4c581f8d42f066f911ddd27ca4b
Prabhat <prabhatkumar944@gmail.com>
2022-12-18 22:07:30 +0530

    brand added

---
+++ src/components/brand/imports.js
@@ -0,0 +1,15 @@
+import atlassian from "../../assets/atlassian.png";
+import dropbox from "../../assets/dropbox.png";
+import shopify from "../../assets/shopify.png";
+import google from "../../assets/google.png";
+import slack from "../../assets/slack.png";
+
+
+export {
+    slack,
+    google,
+    shopify,
+    dropbox,
+    atlassian
+}
+    
